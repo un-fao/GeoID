0ef275f9b40ffd2d0c698e843c43be7cbfe0de0c
Carlo Cancellieri <ccancellieri@hotmail.com>
2026-04-09 02:34:42 +0200

    feat(notebooks): add module lifespan with schema init, seeding, and updated exports

--- src/dynastore/modules/notebooks/__init__.py
+++ src/dynastore/modules/notebooks/__init__.py
@@ -1,11 +1,17 @@
 from .notebooks_module import NotebooksModule
-from .models import NotebookCreate, Notebook, NotebookBase
+from .models import NotebookCreate, Notebook, NotebookBase, PlatformNotebookCreate, PlatformNotebook, OwnerType
 from .notebooks_db import init_notebooks_storage
+from .example_registry import register_platform_notebook, get_registered_notebooks
 
 __all__ = [
-    "NotebooksModule", 
+    "NotebooksModule",
     "init_notebooks_storage",
     "NotebookCreate",
     "Notebook",
-    "NotebookBase"
+    "NotebookBase",
+    "PlatformNotebookCreate",
+    "PlatformNotebook",
+    "OwnerType",
+    "register_platform_notebook",
+    "get_registered_notebooks",
 ]
