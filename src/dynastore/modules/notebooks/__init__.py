076036fe42685abeaba1492cae62461062eb788f
Carlo Cancellieri <ccancellieri@hotmail.com>
2026-04-10 17:08:07 +0200

    notebook fix importing

--- src/dynastore/modules/notebooks/__init__.py
+++ src/dynastore/modules/notebooks/__init__.py
@@ -2,6 +2,7 @@ from .notebooks_module import NotebooksModule
 from .models import NotebookCreate, Notebook, NotebookBase, PlatformNotebookCreate, PlatformNotebook, OwnerType
 from .notebooks_db import init_notebooks_storage
 from .example_registry import register_platform_notebook, get_registered_notebooks
+from . import storage_combo_notebooks  # registers built-in notebooks into the in-memory registry
 
 __all__ = [
     "NotebooksModule",
